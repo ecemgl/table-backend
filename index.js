3f2fb1d1b9d7a95fbfef09f3a9a01f976e0dba05
ecem <ecemgl46@gmail.com>
2023-11-23 01:15:33 +0300

    add port

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const { google } = require('googleapis');
 
 const app = express();
 const cors = require('cors');
+const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
@@ -43,6 +44,9 @@ app.post('/api/feedback', async (req, res) => {
   }
 });
 
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
 
 
 
