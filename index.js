8729514cb3474771a5122893901e5b01af604c43
ecem <ecemgl46@gmail.com>
2023-11-23 01:42:25 +0300

    remove port 3000

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const { google } = require('googleapis');
 
 const app = express();
 const cors = require('cors');
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 
 app.use(cors());
 app.use(express.json());
